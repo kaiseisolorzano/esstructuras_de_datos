8a43d58a2326911c42c8fcc72ec000bbf268ed27
Ale-dx <valalex230w@gmail.com>
2025-11-23 19:21:51 -0600

    Si

---
+++ AppPrincipal.py
@@ -0,0 +1,10 @@
+# =============================================================================
+# Parte prinpcipal para iniciar la aplicación.
+# =============================================================================
+import customtkinter
+
+class App(customtkinter.CTk):
+    pass
+
+
+print("AppPrincipal cargado correctamente.")
